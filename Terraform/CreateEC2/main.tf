a0cd1665f0cfe855962ba108f405deb062fb3a01
chen mardo <chenmardowork@gmail.com>
2025-11-28 11:52:28 +0000

    improve my ec2 script

--- Terraform/CreateEC2/main.tf
+++ Terraform/CreateEC2/main.tf
@@ -33,25 +33,25 @@ resource "tls_private_key" "new_ec2_key" {
   algorithm = "ED25519"
 }
 
-# Upload public key to AWS
+# Upload public key to AWS with a unique name
 resource "aws_key_pair" "new_ec2_key" {
-  key_name   = "new-ec2-key-${timestamp()}"  # unique key name each run
+  key_name   = "new-ec2-key-${timestamp()}"
   public_key = tls_private_key.new_ec2_key.public_key_openssh
 }
 
 # Save the private key locally to ~/.ssh so you can SSH
 resource "local_file" "private_key_file" {
-  content          = tls_private_key.new_ec2_key.private_key_pem
-  filename         = "${pathexpand("~/.ssh/new-ec2-key.pem")}"
-  file_permission  = "0600"
+  content         = tls_private_key.new_ec2_key.private_key_pem
+  filename        = "${pathexpand("~/.ssh/new-ec2-key.pem")}"
+  file_permission = "0600"
 }
 
 # Minimal Ubuntu EC2 instance
 resource "aws_instance" "new_ec2" {
-  ami               = "ami-0fa91bc90632c73c9"  # Ubuntu 22.04 LTS
-  instance_type     = "t2.micro"
-  key_name          = aws_key_pair.new_ec2_key.key_name
-  security_groups   = [aws_security_group.dev_sg.name]
+  ami             = "ami-0fa91bc90632c73c9"  # Ubuntu 22.04 LTS
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.new_ec2_key.key_name
+  security_groups = [aws_security_group.dev_sg.name]
 
   tags = {
     Name = "Terraform-Test-EC2"
@@ -62,3 +62,9 @@ resource "aws_instance" "new_ec2" {
 output "new_ec2_public_ip" {
   value = aws_instance.new_ec2.public_ip
 }
+
+# Output the SSH command ready to copy/paste
+output "ssh_command" {
+  value = "ssh -i ~/.ssh/new-ec2-key.pem ubuntu@${aws_instance.new_ec2.public_ip}"
+}
+
